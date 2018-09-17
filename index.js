e63f4c2b99a123a54f99654dfa051f0ecf380043
Sérgio Ribeiro <sergio.ribeiro.comon@gmail.com>
2018-09-17 01:26:01 +0100

    BaseLines component

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import renderIcon from './src/utils/editor/renderIcon';
 import ComponentHandler from './src/base/ComponentHandler';
 import ComponentInspector from './src/base/ComponentInspector';
 import ColumnGrid from './src/elements/ColumnGrid';
+import BaseLines from './src/elements/BaseLines';
 import './src/scss/main.scss';
 
 // create element for all webmetry components
@@ -12,6 +13,7 @@ document.body.appendChild(wmElement);
 // configure main component handler, and inspector
 const handler = new ComponentHandler(wmElement);
 const inspector = new ComponentInspector(wmElement, handler, [
-	{ title: renderIcon('view_column', 'ColumnGrid'), c: ColumnGrid }
+	{ title: renderIcon('view_column', 'ColumnGrid'), c: ColumnGrid },
+	{ title: renderIcon('view_headline', 'Baselines'), c: BaseLines }
 ]);
 window.wmInstance = { handler, inspector };
