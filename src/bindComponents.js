8f9259c793216873c96ce9cac56f55e67f2b8e60
Sérgio Ribeiro <sergio.r.ribeiro@live.com>
2018-10-14 02:34:10 +0100

    BaseLines are now LineGrid, which inludes pixel grid capabilities

--- src/bindComponents.js
+++ src/bindComponents.js
@@ -3,11 +3,11 @@ import renderIcon from './utils/editor/renderIcon';
 // ALL COMPONENT IMPORTS AND REGISTERS GO BELOW!
 // these will be used by the dialog selector and the import/export utilities
 import ColumnGrid from './elements/ColumnGrid';
-import BaseLines from './elements/BaseLines';
+import LineGrid from './elements/LineGrid';
 import FreeRuler from './elements/FreeRuler';
 
 export default function bindComponents(repo) {
 	repo.register('ColumnGrid', renderIcon('view_column', 'ColumnGrid'), ColumnGrid);
-	repo.register('BaseLines', renderIcon('view_headline', 'BaseLines'), BaseLines);
+	repo.register('LineGrid', renderIcon('view_headline', 'LineGrid'), LineGrid);
 	repo.register('FreeRuler', renderIcon('straighten', 'FreeRuler'), FreeRuler);
 }
