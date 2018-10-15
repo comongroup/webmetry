comongroup/webmetry

commit f59272350a0eb865e330e172f2d345263d8ea828

JSON export now includes inspector state; visibility is now a mode (off/on/auto)

src/base/editor/Inspector.js
@@ -1,4 +1,5 @@
 import debounce from 'lodash/debounce';
+import find from 'lodash/find';
 import map from 'lodash/map';
 import DialogHandler from './DialogHandler';
 import PropertyList from '../../elements/editor/PropertyList';
@@ -45,29 +46,26 @@ export default class Inspector extends DialogHandler {
 			propList.on('change:expanded', () => {
 				this.moveContainerWithinBounds();
 			});
-			propList.on('change:visible', visible => {
-				component.__internalInstance.dom.classList.toggle('-wm-invisible', !visible);
-			});
-			propList.on('check:resize', (width, height) => {
-				propList.state.visible = shouldComponentBeVisible(component, width, height);
+			propList.on('change:visible', () => {
+				const responsiveVisible = shouldComponentBeVisible(component, window.innerWidth, window.innerHeight);
+				const shouldBeVisible = propList.state.visible === 1 || (propList.state.visible === 2 && responsiveVisible);
+				component.__internalInstance.dom.classList.toggle('-wm-invisible', !shouldBeVisible);
 			});
 			propList.on('trash', () => {
 				if (confirm(`Remove this component?`)) {
-					this.handler.remove(propList.state.target);
+					this.handler.remove(component);
 				}
 			});
 			this.add(propList);
 			this.moveContainerWithinBounds();
 			// TODO: need to refactor all of this below
-			const nameHandler = debounce(() => {
+			bindNameEventsTo(component, debounce(() => {
 				propList.emit('change');
-			}, 500);
-			const responsiveHandler = debounce(() => {
-				this.makePropListCheckResize(propList);
-			}, 200);
-			bindNameEventsTo(component, nameHandler);
-			bindResponsiveEventsTo(component, responsiveHandler);
-			responsiveHandler();
+			}, 500));
+			bindResponsiveEventsTo(component, debounce(() => {
+				propList.emit('change:visible');
+			}, 200));
+			propList.emit('change:visible');
 			// TODO: need to refactor all of this above
 		});
 		this.handler.on('remove', components => {
@@ -75,13 +73,10 @@ export default class Inspector extends DialogHandler {
 				const component = components[i];
 				unbindNameEventsFrom(component);
 				unbindResponsiveEventsFrom(component);
-				for (let j = 0; j < this.components.length; j++) {
-					const propList = this.components[j];
-					if (propList.state.target && propList.state.target === component) {
-						this.remove(propList);
-						this.moveContainerWithinBounds();
-						break;
-					}
+				const propList = this.findCorrespondingPropList(component);
+				if (propList) {
+					this.remove(propList);
+					this.moveContainerWithinBounds();
 				}
 			}
 		});
@@ -96,11 +91,13 @@ export default class Inspector extends DialogHandler {
 
 		// on resize?
 		this.onResize = debounce(this.onResize.bind(this), 500);
-		this.onResize();
 		window.addEventListener('resize', this.onResize);
 
 		// append the inspector to its place
 		parent.appendChild(this.container);
+	}
+	findCorrespondingPropList(target) {
+		return find(this.components, propList => propList.state.target && propList.state.target === target);
 	}
 	onResize() {
 		this.moveContainerWithinBounds();
@@ -112,13 +109,8 @@ export default class Inspector extends DialogHandler {
 
 		// make every propList evaluate itself
 		this.components.forEach(component => {
-			component.emit('check:resize', windowWidth, windowHeight);
+			component.emit('change:visible');
 		});
-	}
-	makePropListCheckResize(propList) {
-		const windowWidth = window.innerWidth;
-		const windowHeight = window.innerHeight;
-		propList.emit('check:resize', windowWidth, windowHeight);
 	}
 	setContainerPosition(x, y) {
 		this.container.style.left = x + 'px';
@@ -175,7 +167,7 @@ export default class Inspector extends DialogHandler {
 			]
 		}).on('select', ({ action, source }) => {
 			if (action === 'import') {
-				const input = prompt('Paste the JSON here.\nThis will clear current components.');
+				const input = prompt('Paste the JSON here.\nThis will replace all current components.');
 				if (input) {
 					try {
 						const arr = JSON.parse(input);
@@ -192,7 +184,14 @@ export default class Inspector extends DialogHandler {
 									console.warn(`component ${entry} not found`);
 								}
 								else {
-									this.handler.add(new entry.Constructor(obj.options), obj.type);
+									const component = new entry.Constructor(obj.options || {});
+									this.handler.add(component, obj.type);
+									if (obj.list) {
+										const propList = this.findCorrespondingPropList(component);
+										if (propList) {
+											Object.assign(propList.state, obj.list || {});
+										}
+									}
 								}
 							});
 						}
@@ -209,10 +208,16 @@ export default class Inspector extends DialogHandler {
 				}
 			}
 			else if (action === 'export' && source === 'json') {
-				const arr = map(this.handler.components, c => ({
-					type: c.__internalId,
-					options: c.serialize()
-				}));
+				const arr = map(this.handler.components, component => {
+					const optionsObject = component.serialize(true);
+					const list = this.findCorrespondingPropList(component);
+					const listObject = list ? list.serialize(true) : {};
+					return {
+						type: component.__internalId,
+						options: optionsObject,
+						list: listObject
+					};
+				});
 				const output = JSON.stringify(arr, null, '\t');
 				prompt('Here is the JSON code', output);
 			}

src/base/Component.js
@@ -1,5 +1,6 @@
 import defaultsDeep from 'lodash/defaultsDeep';
 import mapValues from 'lodash/mapValues';
+import pickBy from 'lodash/pickBy';
 import diffObject from '../utils/diffObject';
 import Emitter from '../utils/Emitter';
 import observeObject from '../utils/observeObject';
@@ -19,7 +20,7 @@ export default class Component extends Emitter {
 
 		// state
 		const state = defaultsDeep(options || {}, mapValues(this.props, o => o.default || undefined));
-		this.serialize = () => this.internalSerialize(state);
+		this.serialize = delta => this.internalSerialize(state, delta);
 		this.unserialize = data => this.internalUnserialize(state, data);
 
 		// observe state
@@ -35,10 +36,14 @@ export default class Component extends Emitter {
 	rendered(dom) {}
 	mounted(dom) {}
 	unmounted(dom) {}
-	internalSerialize(state) {
-		// for now, just return the state as it is,
-		// but make a shallow copy of it
-		return { ...state };
+	internalSerialize(state, delta) {
+		// return the state of the object,
+		// but filter out keys that are not props,
+		// or values that equal the default (if delta == true)
+		return pickBy(state, (v, k) => {
+			const prop = this.props[k];
+			return prop && !prop.omit && (delta !== true || prop.default !== v);
+		});
 	}
 	internalUnserialize(state, data) {
 		const diff = diffObject(state, data);

src/elements/editor/PropertyList.jsx
@@ -5,9 +5,9 @@ import renderPropEditor from '../../utils/editor/renderPropEditor';
 export default class PropertyList extends Component {
 	constructor(options) {
 		super(options, {
-			target: { type: Component },
-			visible: { type: Boolean, default: true },
-			expanded: { type: Boolean, default: true }
+			target: { type: Component, omit: true },
+			expanded: { type: Boolean, default: true },
+			visible: { type: Number, default: 2 }
 		});
 	}
 	render() {
@@ -38,11 +38,17 @@ export default class PropertyList extends Component {
 						: <i className="material-icons">keyboard_arrow_down</i>}
 				</span>
 				<span className="-wmfl-title">{target.getName()}</span>
-				<span className="-wmfl-option -wmfl-on-hover" title="Delete component" onClick={e => this.deleteTarget(e)}>
+				<span className="-wmfl-option -wmfl-on-hover" title="Delete component" onClick={e => this.trashTarget(e)}>
 					<i className="material-icons">delete_outline</i>
 				</span>
-				<span className="-wmfl-option" title="Toggle visibility" onClick={e => this.toggleTargetVisibility(e)}>
-					<i className="material-icons">{this.state.visible === false ? 'visibility_off' : 'visibility'}</i>
+				<span className="-wmfl-option" title="Toggle visibility mode" onClick={e => this.toggleVisibilityMode(e)}>
+					<i className="material-icons">
+						{this.state.visible === 0
+							? 'flash_off'
+							: this.state.visible === 1
+								? 'flash_on'
+								: 'flash_auto'}
+					</i>
 				</span>
 			</div>
 			<div className="wm-property-list-props">{children}</div>
@@ -76,13 +82,14 @@ export default class PropertyList extends Component {
 		this.emit('input', target, prop, convertedValue);
 	}
 	toggleExpandedState(e) {
+		e.stopPropagation();
 		this.state.expanded = !this.state.expanded;
 	}
-	toggleTargetVisibility(e) {
+	toggleVisibilityMode(e) {
 		e.stopPropagation();
-		this.state.visible = !this.state.visible;
+		this.state.visible = (this.state.visible + 1) % 3;
 	}
-	deleteTarget(e) {
+	trashTarget(e) {
 		e.stopPropagation();
 		this.emit('trash');
 	}

src/scss/main.scss
@@ -70,6 +70,7 @@ body {
 			&.-wmfl-title {
 				margin-right: auto;
 				padding: 10px 5px;
+				padding-left: 0;
 				pointer-events: none;
 
 				&:first-child {
@@ -87,7 +88,7 @@ body {
 
 		&:not(:hover) {
 			.-wmfl-on-hover {
-				display: none;
+				visibility: hidden;
 			}
 		}
 	}

commit 0d3f537bdb8f0caf30e8c77d0d66a012ec526450

Fixing bug introduced in f59272350a0eb865e330e172f2d345263d8ea828, webmetry wouldn't calc viewport on init

src/base/editor/Inspector.js
@@ -123,7 +123,7 @@ export default class Inspector extends DialogHandler {
 		// set container position
 		this.state.snappedToBottom = false;
 		this.setContainerPosition(0, 0);
-		this.moveContainerWithinBounds();
+		this.onResize();
 
 		// on resize?
 		this.onResize = debounce(this.onResize.bind(this), 500);